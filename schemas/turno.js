d19f41ff8542e4848c35db70df2c035c32e7bdcd
Migsayal <>
2023-12-04 19:19:28 -0300

    cambio de nombre mascota id

--- schemas/turno.js
+++ schemas/turno.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const { ObjectId } = require('mongodb');
 
 const turnoSchema = new mongoose.Schema({
-    fechaTurno: {
+  fechaTurno: {
     type: String,
     required: true,
   },
@@ -17,12 +18,17 @@ const turnoSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
+  mascota_id: {
+    type: ObjectId,
+    ref: 'Mascota',
+    required: true,
+  },
   estado: {
     type: String,
     required: true,
-  }
-});
+  },
+})
 
-const Turno = mongoose.model('Turnos', turnoSchema);
+const Turno = mongoose.model('Turnos', turnoSchema)
 
-module.exports = Turno;
+module.exports = Turno
